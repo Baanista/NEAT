7577f2bf52ea19bf56d0d84a4915f98d38294a4d
Baanista <110647786+Baanista@users.noreply.github.com>
2023-02-06 16:03:50 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,22 +9,44 @@
 using namespace std;
 #include<cstdlib>
 #include<random>
-#include "NNetwork.hpp"
+//#include "NNetwork.hpp"
+#include "RNetwork.hpp"
 #include <time.h>
 #include <stdio.h>      
+#include <vector>
+
+vector<vector<vector<double> > > zerostart(vector<int> structure)
+{
+    int layers = structure.size();
+    vector<vector<vector<double> > > output(layers);
+
+    for (int i = 0; i < layers; i++)
+    {
+        int nodes = structure[i];
+        output[i].resize(nodes);
+
+        for (int j = 0; j < nodes; j++)
+        {
+            output[i][j].resize(2);
+            output[i][j][0] = 0.0;
+            output[i][j][1] = 0.0;
+        }
+    }
+
+    return output;
+}
 
 
-int main() {
-    
-    
-    RNetwork RNet;
-    RNet.Brain = zerostart;
+
+int main()
+{
+    RNetworker RNet;
+
+    vector<int> bstruct = {1, 2, 1};
+    vector<vector<vector<double> > > temp = zerostart(bstruct);
+    RNet.Brain = temp;
 
     cout << RNet.Brain[0][0][0] << endl;
-        // Print the random number
-    //cout<<v2<<endl;
-    
-    //cout << Brain.;
-    
-    
+//
+    return 0;
 }
