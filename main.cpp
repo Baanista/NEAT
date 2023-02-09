ee7a093b6a9f541c16146580fbefe1da28e00f3d
Baanista <110647786+Baanista@users.noreply.github.com>
2023-02-08 16:04:55 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,22 +15,27 @@ using namespace std;
 #include <stdio.h>      
 #include <vector>
 
-vector<vector<vector<double> > > zerostart(vector<int> structure)
+vector<vector<vector<vector<double> > > > zerostart(vector<int> structure)
 {
     int layers = structure.size();
-    vector<vector<vector<double> > > output(layers);
-
-    for (int i = 0; i < layers; i++)
+    vector<vector<vector<vector<double> > > > output;
+    output.resize(structure.size() - 1);
+    for (int i = 0; i < layers -1; i++)
     {
+        
         int nodes = structure[i];
-        output[i].resize(nodes);
-
-        for (int j = 0; j < nodes; j++)
+        output[i].resize(structure[i + 1]);
+        for (int a = 0; a < structure[i + 1]; a++)
         {
-            output[i][j].resize(2);
-            output[i][j][0] = 0.0;
-            output[i][j][1] = 0.0;
-        }
+            output[a].resize(nodes + 1);
+            for (int j = 0; j < structure[i]; j++)
+            {
+                
+                output[i][j][a].resize(2);
+                output[i][j][a][0] = 0.0;
+                output[i][j][a][1] = 0.0;
+            }
+    }
     }
 
     return output;
@@ -47,11 +52,13 @@ int main()
     bstruct.push_back(2);
     bstruct.push_back(1);
 
-    vector<vector<vector<double> > > temp = zerostart(bstruct);
-    RNet.Brain = temp;
+
+    printf("hello");
+    
+    vector<vector<vector<vector<double> > > > temp = zerostart(bstruct);
+    // RNet.Brain = temp;
     
 
-    cout << RNet.Brain[0][0][0] << endl;
+    // cout << RNet.Brain[0][0][0][0] << endl;
 
-    return 0;
 }
