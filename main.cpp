8ead4954091eb16e90ebf8d0e9b51546bededbb3
Baanista <110647786+Baanista@users.noreply.github.com>
2023-02-15 15:57:19 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ using namespace std;
 #include <stdio.h>      
 #include <vector>
 #include <iomanip>
+
 vector<vector<vector<vector<double> > > > zerostart(vector<int> structure)
 {
     int layers = structure.size();
@@ -32,8 +33,42 @@ vector<vector<vector<vector<double> > > > zerostart(vector<int> structure)
             {
                 
                 output[i][a][j].resize(2);
-                output[i][a][j][0] = i + a * j;
-                output[i][a][j][1] = i + a * j +1;
+                output[i][a][j][0] = 0;
+                output[i][a][j][1] = 0;
+            }
+    }
+    }
+
+    return output;
+}
+
+float randomWeight()
+{
+    return((float(rand())/float((RAND_MAX))));
+}
+
+vector<vector<vector<vector<double> > > > randstart(vector<int> structure)
+{
+    int layers = structure.size();
+    vector<vector<vector<vector<double> > > > output;
+    output.resize(layers - 1);
+    for (int i = 0; i < layers -1; i++)
+    {
+        
+        int nodes = structure[i];
+        output[i].resize(structure[i + 1]);
+        for (int a = 0; a < structure[i+1]; a++)
+        {
+            output[i][a].resize(nodes + 1);
+            
+            for (int j = 0; j < structure[i]; j++)
+            {
+                
+                output[i][a][j].resize(2);
+                
+                output[i][a][j][0] = randomWeight() * 2;
+                
+                output[i][a][j][1] = randomWeight() * 2;
             }
     }
     }
@@ -70,15 +105,15 @@ int main()
     RNetworker RNet;
 
     vector<int> bstruct;
+    bstruct.push_back(1);
     bstruct.push_back(3);
-    bstruct.push_back(3);
-    bstruct.push_back(3);
+    bstruct.push_back(1);
     
 
 
     
-    
-    vector<vector<vector<vector<double> > > > temp = zerostart(bstruct);
+    srand((unsigned int)time(NULL));
+    vector<vector<vector<vector<double> > > > temp = randstart(bstruct);
     RNet.Brain = temp;
     
 
@@ -86,12 +121,13 @@ int main()
 
     vector<double> input;
     input.push_back(1);
-    input.push_back(1);
-    input.push_back(1);
+
     
     printneuralnetwork(RNet.Brain);
+    print_vector(input);
+    vector<double> output = RNet.plugin_info(input);
     
-    //vector<double> output = RNet.plugin_info(input);
-    //print_vector(input);
-    //print_vector(output);
+    print_vector(output);
+    double score = RNet.scorer(input, input);
+    cout << "score:" << score << endl;
 }
